75ee6606242c8f75df17f8838c3702c27c25f078
jmcalama <jmcalama@hotmail.com>
2014-12-23 22:59:30 +0100

    Commit 1

---
+++ OneASP.NET_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneASP.NET_1.Startup))]
+namespace OneASP.NET_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
